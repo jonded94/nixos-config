d5266ef6bf9bc362610996abdb78185ffd8dae3e
Jonas Dedden <jonas.dedden@deepl.com>
2025-11-08 20:35:27 +0100

    Remove `system.copySystemConfiguration` for now

--- configuration.nix
+++ configuration.nix
@@ -86,8 +86,6 @@
 
   networking.firewall.enable = false;
 
-  system.copySystemConfiguration = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
